9fad95a070a4004af24e97cd5adff7495255267a
Adedrius <adeegorp@gmail.com>
2026-07-14 20:32:58 -0400

    Testing complete

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       <div className='my-10 flex flex-col md:flex-row gap-16'>
           <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
           <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
-              <p>Cenchhhhh Forever was created from a passion for innovation and a goal to transform online shopping. Our journey started with a simple vision: to offer a platform where customers can easily browse, explore, and buy a variety of products from the comfort of their own homes.</p>
+              <p>Forever was created from a passion for innovation and a goal to transform online shopping. Our journey started with a simple vision: to offer a platform where customers can easily browse, explore, and buy a variety of products from the comfort of their own homes.</p>
               <p>Since our launch, we've dedicated ourselves to curating a diverse range of top-quality products to suit every taste and need. From fashion and beauty to electronics and home essentials, we offer an extensive selection from trusted brands and suppliers.</p>
               <b className='text-gray-800'>Our Mission</b>
               <p>At Forever, our mission is to give customers the power of choice, convenience, and trust. We are committed to offering a smooth shopping journey that goes beyond expectations, from browsing and ordering to delivery and beyond.</p>
